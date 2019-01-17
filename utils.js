058bd36eeff56d986d71d1752f41d579bf7db71e
Jeffrey <djefre@users.noreply.github.com>
2019-01-17 16:13:44 +0100

    Add files via upload
    Update

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ function loadExternalJsFileByUrl(url){
 	document.head.appendChild(scriptElement);
 }
 
-// ==================================================================
+/* ================================================================== */
 
 // Executes a supplied function when the DOM is loaded
 function onDomLoaded(onReady){
@@ -16,11 +16,28 @@ function onDomLoaded(onReady){
 	}
 }
 
-// ==================================================================
+/* ================================================================== */
 
-// function downloadFileByUrl(url, fileName){
-//     let link = document.createElement("a");
-//     link.download = name;
-//     link.href = url;
-//     link.click();
-// }
+function fireFunctionWhenUserStopsTyping(elementSelector, interval, functionToExecuteWhenUserIsDoneTyping) {
+	let timer;
+	let inputElement = document.querySelector(elementSelector);
+
+	// start the countdown on 'keyup'
+	inputElement.addEventListener('keyup', function () {
+		clearTimeout(timer);
+		if (inputElement.value) {
+			timer = setTimeout(functionToExecuteWhenUserIsDoneTyping, interval);
+		}
+	});
+}
+
+/* ================================================================== */
+
+/*function downloadFileByUrl(url, fileName){
+     let link = document.createElement("a");
+    link.download = name;
+     link.href = url;
+     link.click();
+ } */
+
+/* ================================================================== */
