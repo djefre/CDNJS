77f13afe994d40e509c81edd6d5df643c3e333d7
Jeffrey <djefre@users.noreply.github.com>
2019-01-17 10:40:31 +0100

    Add files via upload
    Initial Commit

---
+++ httpclient.js
@@ -0,0 +1,32 @@
+// Promise based httpClient
+var httpClient = function (method, url) {
+	return new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+    	xhr.open(method, url);
+    	xhr.onload = function () {
+	      	if (this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+	      	} 
+	      	else {
+        		reject({
+          			status: this.status,
+          			statusText: xhr.statusText
+        		});
+ 			}
+    	};
+    	xhr.onerror = function () {
+      		reject({
+        		status: this.status,
+        		statusText: xhr.statusText
+      		});
+    	};
+   		xhr.send();
+	});
+}
+
+// === EXAMLPLE ===
+// httpClient('GET', testUrl)
+// 	.then((r) =>{ console.log(r); })
+// 	.catch(function (err) {
+// 	  console.error("I've encountered an error ): ", err.statusText);
+// 	});
