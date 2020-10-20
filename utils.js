9760fa2e23e4cb2f519ffdb9c4ddf7f3fbc8c649
J <djefre@users.noreply.github.com>
2020-10-20 11:07:29 +0200

    Update utils.js
    sytax fix

--- utils.js
+++ utils.js
@@ -81,7 +81,7 @@ function rainbowifyElementBackground(cssSelector, interval) {
 
 /* ================================================================== */
 
-getDirectGiphyLink() {
+function getDirectGiphyLink() {
     if(!window.location.href.includes(`giphy.com`)) return
 
     let gifId = undefined
